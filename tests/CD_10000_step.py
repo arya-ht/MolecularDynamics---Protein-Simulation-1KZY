9acd8f41bc2be57fa9b160a08865fa155603204f
Arya H <aryaht23@gmail.com>
2017-08-10 15:04:58 -0500

    added CD test case for 10000 steps

---
+++ tests/CD_10000_step.py
@@ -0,0 +1,27 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+f = open('CD_10000_STEP.csv', 'w')#file to write chain data in
+print('Loading...')
+pdb = PDBFile('CD.pdb')
+forcefield = ForceField('amber99sb.xml', 'tip3p.xml')#create forcefield
+modeller = Modeller(pdb.topology, pdb.positions)#initialize modeller
+print('Adding hydrogens...')
+modeller.addHydrogens(forcefield)
+print('Adding solvent...')
+modeller.addSolvent(forcefield, model='tip3p', padding=1*nanometer)#neutralize the system with ions
+print('Minimizing...')
+system = forcefield.createSystem(modeller.topology, nonbondedMethod=PME)
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.001*picoseconds)
+platform = Platform.getPlatformByName('OpenCL')
+simulation = Simulation(modeller.topology, system, integrator, platform)
+simulation.context.setPositions(modeller.positions)
+simulation.minimizeEnergy()
+#simulation.reporters.append(PDBReporter('CDtest.pdb', 1000))
+print('Saving...')
+
+simulation.reporters.append(StateDataReporter(f, 10000, step=True,kineticEnergy=True,
+                                              potentialEnergy=True,totalEnergy = True,temperature=True,density = True))
+#for every 100 steps, write into f 
+simulation.step(120000)#run for x steps
+print('done')
